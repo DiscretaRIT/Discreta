8a46a65b71e167c00f240a3cca3eeaf05ff11e69
sathya-pramodh <sathyapramodh17@gmail.com>
2024-11-17 17:51:42 +0530

    feat: Add mysql connection and dotenv.

--- api/main.py
+++ api/main.py
@@ -1,7 +1,31 @@
+from posix import error
 from fastapi import FastAPI
+import mysql.connector as msql
+import dotenv
+import os
 
-# Create FastAPI instance with custom docs and openapi url
+dotenv.load_dotenv()
+
+env_type = os.getenv("RUN_ENV")
+
 app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+
+user = os.getenv("DB_USER")
+if user is None:
+    error("No DB_USER environment variable found!")
+    exit(1)
+
+password = os.getenv("DB_PASSWORD")
+if password is None:
+    error("No DB_PASSWORD environment variable found!")
+    exit(1)
+
+db = msql.connect(
+    user=user,
+    password=password,
+    host="localhost",
+    database="Discreta"
+)
 
 
 @app.get("/api/hello")
